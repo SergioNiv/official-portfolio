8a9cbe74fc91a461a981ac26e94d9d83af320c29
Sergio Nivin <nivin684@gmail.com>
2021-10-07 11:23:12 -0500

    test with _blank

--- components/main/Aside.jsx
+++ components/main/Aside.jsx
@@ -15,10 +15,15 @@ const Aside = () => {
 				<a
 					href="https://www.linkedin.com/in/sergio-nivin"
 					className="aside-link"
+					target="_blank"
 				>
 					<i className="fab fa-linkedin"></i> Linkedin
 				</a>
-				<a href="https://github.com/SergioNiv" className="aside-link">
+				<a
+					href="https://github.com/SergioNiv"
+					className="aside-link"
+					target="_blank"
+				>
 					<i className="fab fa-github"></i> GitHub
 				</a>
 				<a href="/" className="aside-link">
